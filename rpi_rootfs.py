76b28afcaf30535b1aafbb56370128e955d3db20
Dan Rosser <danoli3@gmail.com>
2025-01-09 01:24:42 +1100

    fix

--- rpi_rootfs.py
+++ rpi_rootfs.py
@@ -187,13 +187,13 @@ def inplace_change(filename, old_string, new_string):
     with open(filename) as f:
         s = f.read()
         if old_string not in s:
-            print '"{old_string}" not found in {filename}.'.format(**locals())
+            print('"{old_string}" not found in {filename}.'.format(**locals()))
             return
 
     try:
         # Safely write the changed content, if found in the file
         with open(filename, 'w') as f:
-            print 'Changing "{old_string}" to "{new_string}" in {filename}'.format(**locals())
+            print ('Changing "{old_string}" to "{new_string}" in {filename}'.format(**locals()))
             s = s.replace(old_string, new_string)
             f.write(s)
     except OSError, e:
