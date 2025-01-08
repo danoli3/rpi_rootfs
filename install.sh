7c5c5c7d8918dd16fea831c2fa1272c1ea30242c
Dan Rosser <danoli3@gmail.com>
2025-01-08 20:31:17 +1100

    python to python3

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 
 sudo apt update && sudo apt dist-upgrade -y
-sudo apt-get install -y build-essential gawk gcc g++ gfortran git texinfo bison libncurses-dev tar wget qemu-user-static rsync xz-utils unzip python3 python3-pip
+sudo apt-get install -y build-essential gawk gcc g++ gfortran git texinfo bison libncurses-dev tar wget qemu-user-static rsync xz-utils unzip python3 python3-pip python-is-python3
