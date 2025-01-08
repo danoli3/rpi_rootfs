4f14f5eae0da55845b6824521ee5a2db2e45057b
Dan Rosser <danoli3@gmail.com>
2025-01-08 19:48:37 +1100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPT_DIR
+
+sudo apt update && sudo apt dist-upgrade -y
+sudo apt-get install -y build-essential gawk gcc g++ gfortran git texinfo bison libncurses-dev tar wget qemu-user-static rsync xz-utils unzip
